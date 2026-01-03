2f891442410f62bb0d4f452db4c11a2f0ee6a9f1
AshotWebDev <poghosyan.01@list.ru>
2026-01-03 23:07:23 +0400

    change

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Header/>
       <h2 className="title text-center text-2xl md:text-3xl !font-extralight !mt-10 uppercase">Օրվա Ծրագիրը</h2>
       <Section icon={ringsIcon} time="15:30" title="Պսակադրություն" locationName="Արտաշատի Սուրբ Հովհանես Եկեղեցի" img={churchImg} address="Հասցեն՝ Արարատի մարզ   ք․ Արտաշատ" locationLink="https://yandex.com/maps/geo/2496643584/?ll=46.164441%2C38.873114&z=13.81"/>
-      <Section icon={champagnesIcon} time="18:00" title="Հարսանյաց Հանդիսություն" locationName="Մորենա ռեստորանային համալիր" img={restaurantImg} address="Հասցեն՝ Մասիս, Մխիթար Հերացու փող., 7/8" locationLink="https://yandex.com/maps/org/222913353582/?ll=44.444778%2C40.059849&z=16.77"/>
+      <Section icon={champagnesIcon} time="17:30" title="Հարսանյաց Հանդիսություն" locationName="Մորենա ռեստորանային համալիր" img={restaurantImg} address="Հասցեն՝ Մասիս, Մխիթար Հերացու փող., 7/8" locationLink="https://yandex.com/maps/org/222913353582/?ll=44.444778%2C40.059849&z=16.77"/>
       <div className='w-[200px] h-[1px] bg-gray-400 !mx-auto !my-10'></div>
       <Contact/>
       <Footer/>
